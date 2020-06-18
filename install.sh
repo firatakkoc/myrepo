b40abc0902f2bbbca2fe2bea4037a7a5f895b7ec
Firat Akkoc <firat.akkoc@Modanisas-MacBook-Pro.local>
2020-06-18 12:02:15 +0300

    2.

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!bin/bash
 
 
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install git
-git clone git@github.com:firalstakkoc/myrepo.git
+
 sudo apt -y install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
 sudo apt -y install python3-virtualenv python3-pip python3-dev python3-venv
 sudo apt -y install python-virtualenv 
