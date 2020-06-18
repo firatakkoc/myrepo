4365c957967507daf1a39ad82ae6a49aacfdc5cf
Firat Akkoc <firat.akkoc@Modanisas-MacBook-Pro.local>
2020-06-18 11:24:01 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
+sudo apt -y install python3-virtualenv python3-pip python3-dev python3-venv
+sudo apt -y install python-virtualenv 
+pip3 install flask gunicorn virtualenv flask-sqlalchemy
+sudo apt -y install nginx
+# Folder Commands
+mkdir /home/vagrant/ todo
+cd /todo
+virtualenv todo_env
